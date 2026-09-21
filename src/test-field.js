a1fe04133df849db82f495a53bc75b72b403b6dc
Sofia Andersson <sa226jf@student.lnu.se>
2026-09-21 10:16:59 +0200

    Refactor test for Field class to separate valid and duplicate rule scenarios

--- src/test-field.js
+++ src/test-field.js
@@ -1,14 +1,26 @@
 import Field from './Field.js'
 import Rule from './Rule.js'
 
-/**
- * This test will throw an error because we are trying to add a rule of the same type to the same field.
- */
-
 const email = new Field('email', 'required')
 
 email.addRule(new Rule('required', 'Email is required'))
 
-email.addRule(new Rule('required', 'Email is required'))
+email.addRule(new Rule('email', 'Email must be a valid email address'))
+
+console.log('Adding rules to the email field')
 
 console.log(email)
+
+/**
+ * This test will throw an error because we are trying to add a rule of the same type to the same field.
+ */
+
+const emailWithDuplicateRules = new Field('email', 'required')
+
+emailWithDuplicateRules.addRule(new Rule('required', 'Email is required'))
+
+emailWithDuplicateRules.addRule(new Rule('required', 'Email is required'))
+
+console.log('Duplicate rule test: Adding a rule of the same type to the same field should throw an error')
+
+console.log(emailWithDuplicateRules)
