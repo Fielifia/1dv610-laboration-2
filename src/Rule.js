ddd7c52449dfb0cda278ff299d9203e35e349558
Sofia Andersson <sa226jf@student.lnu.se>
2026-09-23 12:49:00 +0200

    Add validation rules for OnlyLetters, OnlyDigits, MinLength, and MaxLength; update tests for new rules

--- src/Rule.js
+++ src/Rule.js
@@ -22,3 +22,92 @@ export class EmailRule extends Rule {
     return emailRegex.test(data)
   }
 }
+
+export class OnlyLettersRule extends Rule {
+  validate(data) {
+    const letterRegex = /^\p{L}*$/u
+    return letterRegex.test(data)
+  }
+}
+
+export class OnlyDigitsRule extends Rule {
+  validate(data) {
+    const digitRegex = /^\d+$/
+    return digitRegex.test(data)
+  }
+}
+
+export class MinLengthRule extends Rule {
+  constructor(minLength, errorMessage) {
+    super(errorMessage)
+    this.minLength = minLength
+  }
+
+  validate(data) {
+    return data.length >= this.minLength
+  }
+}
+
+export class MaxLengthRule extends Rule {
+  constructor(maxLength) {
+    this.maxLength = maxLength
+  }
+
+  validate(data) {
+    return data.length <= this.maxLength
+  }
+}
+
+export class MinValueRule extends Rule {
+  constructor(minValue) {
+    this.minValue = minValue
+  }
+
+  validate(data) {
+    return Number(data) >= this.minValue
+  }
+}
+
+export class MaxValueRule extends Rule {
+  constructor(maxValue) {
+    this.maxValue = maxValue
+  }
+
+  validate(data) {
+    return Number(data) <= this.maxValue
+  }
+}
+
+
+export class UppercaseRule extends Rule {
+  validate(data) {
+    const uppercaseRegex = /\p{Lu}/u
+    return uppercaseRegex.test(data)
+  }
+}
+
+export class LowercaseRule extends Rule {
+  validate(data) {
+    const lowercaseRegex = /\p{Ll}/u
+    return lowercaseRegex.test(data)
+  }
+}
+
+export class SpecialCharacterRule extends Rule {
+  validate(data) {
+    const specialCharacterRegex = /[^\p{L}\d\s]/u
+    return specialCharacterRegex.test(data)
+  }
+}
+
+export class NoCodeOrLinksRule extends Rule {
+  validate(data) {
+    const codeRegex = /<[^>]*>|javascript:/i
+    const linkRegex = /(https?:\/\/[^\s]+|www\.[^\s]+|[a-zA-Z0-9-]+\.[a-zA-Z]{2,})/i
+    return (codeRegex.test(data) && linkRegex.test(data))
+  }
+}
+
+export class MatchingFieldsRule extends Rule {
+  // Logic
+}
--- src/test-validation.js
+++ src/test-validation.js
@@ -1,5 +1,5 @@
 import Field from './Field.js'
-import { RequiredRule, EmailRule } from './Rule.js'
+import { RequiredRule, EmailRule, MinLengthRule, OnlyLettersRule, OnlyDigitsRule } from './Rule.js'
 import Validation from './Validation.js'
 
 // Test required
@@ -29,3 +29,33 @@ const emailFormatValidation = new Validation()
 emailFormatValidation.addField(emailFormat)
 
 console.log(emailFormatValidation.validate({ emailFormat: 'sofia@hotmail.com' }))
+
+
+// Test min length format
+
+console.log('Test min length: ')
+
+const name = new Field('name')
+
+name.addRule(new MinLengthRule(2, 'Must be at least 2 characters'))
+
+name.addRule(new OnlyLettersRule('Only letters accepted'))
+
+const minLengthValidation = new Validation()
+
+minLengthValidation.addField(name)
+
+console.log(minLengthValidation.validate({ name: 'Bo!' }))
+
+
+console.log('Test only digits rule: ')
+
+const age = new Field('age')
+
+age.addRule(new OnlyDigitsRule('Must be digits'))
+
+const onlyDigitsValidation = new Validation()
+
+onlyDigitsValidation.addField(age)
+
+console.log(onlyDigitsValidation.validate({ age: '8' }))
